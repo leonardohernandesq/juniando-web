da723ec35a698221142b523bc94942473342c042
lucaslimasz <id.lucas@outlook.com.br>
2024-12-22 00:58:39 -0300

    style: colors, plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        light: "#FFF",
+        principal: "#0565FF",
+
+        "dark-100": "#202020",
+      },
+      dropShadow: {
+        principal: "0 1px 10px rgba(0, 0, 0, 0.25)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 } satisfies Config;
