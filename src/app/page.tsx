8c35486f45d2c1a8dca6d93dfd8f9a1ff3732500
lucaslimasz <id.lucas@outlook.com.br>
2024-12-22 01:00:21 -0300

    test: test page for the card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Card from "@/components/card";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="max-w-5xl m-auto grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+      <Card
+        author="Lucas Lima"
+        date={new Date()}
+        title="Por onde começar a estudar programação?"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+      />
+      <Card
+        author="Lucas Lima"
+        date={new Date()}
+        title="Por onde começar a estudar programação?"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+      />
+      <Card
+        author="Lucas Lima"
+        date={new Date()}
+        title="Por onde começar a estudar programação?"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+      />
+      <Card
+        author="Lucas Lima"
+        date={new Date()}
+        title="Por onde começar a estudar programação?"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+      />
     </div>
   );
 }
