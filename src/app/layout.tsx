cd89c4f8b2183e7a7b4fec77770b5d118c2dc1b2
Leonardo Hernandes <62108109+leonardohernandesq@users.noreply.github.com>
2024-12-27 19:47:43 +0000

    refactor: layout and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/footer";
+import Footer from "../components/footer";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
