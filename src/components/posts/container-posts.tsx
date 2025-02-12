403ab73d53157ef7f80881cee536fdaa7d0be457
Daniel San <danielgdt88@hotmail.com>
2025-02-12 11:27:54 -0300

    style: finalizo mobile e organizo classes tailwind nos arquivos

--- src/components/posts/container-posts.tsx
+++ src/components/posts/container-posts.tsx
@@ -8,9 +8,9 @@ const BlogPosts = React.lazy(() => import("./blog-posts"));
 
 export default function ContainerPosts() {
   return (
-    <main className="px-[155px] pt-9 grid lg:grid-cols-[65%_35%] relative md:grid-cols-1 sm:grid-cols-1">
+    <main className="pt-9 grid relative px-4 md:grid-cols-1 md:px-[155px] lg:grid-cols-[65%_35%] lg:px-[155px">
       <BlogPosts />
-      <div className="flex lg:flex-col gap-y-6 items-end md:flex-row md:gap-x-4 md:mb-4">
+      <div className="flex flex-col gap-y-6 items-center mb-4 md:items-end md:flex-row md:gap-x-4 md:mb-4 lg:items-end lg:flex-col ">
         <ArticleObjetivoPosts title="Juniando">
           Ajudamos você Desenvolvedor Jr a conseguir sua primeira vaga no
           mercado de trabalho, com dicas e anúncios de vagas para você alavangar
@@ -18,7 +18,7 @@ export default function ContainerPosts() {
         </ArticleObjetivoPosts>
 
         <ArticleObjetivoPosts title="Assuntos Relevantes">
-          <div className="w-24 h-[2px] rounded-2xl bg-[#0565FF] lg:mx-auto lg:my-1 md:m-0 md:mx-auto"></div>
+          <div className="mx-auto w-24 h-[2px] rounded-2xl bg-[#0565FF] md:m-0 md:mx-auto lg:mx-auto lg:my-1"></div>
           <TopicPosts />
         </ArticleObjetivoPosts>
       </div>
