ae8f1af7b139bbc05ab1bcdd1947ce3ac4fd2691
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2025-02-16 21:33:47 +0000

    create about us page and box component

--- src/components/mission-box.tsx
+++ src/components/mission-box.tsx
@@ -10,12 +10,7 @@ interface IMissionBox {
 const MissionBox = ({ icon, alt, title, description }: IMissionBox) => {
   return (
     <div className="w-full p-5 border border-light-gray-200 rounded-2xl shadow-lg">
-      <Image
-        src={`/images/${icon}`}
-        width={50}
-        height={50}
-        alt={alt || "Mission Icon"}
-      />
+      <Image src={`/images/${icon}`} width={50} height={50} alt={alt} />
       <h3 className="font-bold text-xl my-3 text-dark-100">{title}</h3>
       <div className="text-gray-600">{description}</div>
     </div>
