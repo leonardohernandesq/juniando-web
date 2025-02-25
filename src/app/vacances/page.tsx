8db56fcef88bb5bdd440a1c5cad769bcb27e6084
Daniel San <danielgdt88@hotmail.com>
2025-02-25 17:33:56 -0300

    feat: create component for job search

--- src/app/vacances/page.tsx
+++ src/app/vacances/page.tsx
@@ -1,4 +1,5 @@
 import IResponseJson from "@/models/response-json.interface";
+import Image from "next/image";
 
 export default async function PageVacances() {
   const response = await fetch(
@@ -15,9 +16,23 @@ export default async function PageVacances() {
   const json: IResponseJson = await response.json();
   console.log(json.record.posts);
   return (
-    <div className="bg-blue-dark text-center text-4xl py-20 text-white font-semibold">
-      Vagas para <br />{" "}
-      <span className="text-yellow-title">Desenvolvedores Jr</span>
+    <div>
+      <div className="bg-blue-dark text-center text-4xl py-20 text-white font-semibold mb-11">
+        Vagas para <br />{" "}
+        <span className="text-yellow-title">Desenvolvedores Jr</span>
+      </div>
+
+      <div className="flex border border-dark-blue w-[468px] h-12 items-center justify-between rounded-full px-8 mb-11 mx-auto">
+        <input type="text w-full" placeholder="Pesquise por vagas..." />
+        <span>
+          <Image
+            src={"/images/search.png"}
+            width={24}
+            height={24}
+            alt="search"
+          />
+        </span>
+      </div>
     </div>
   );
 }
