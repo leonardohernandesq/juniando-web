899133495bcde9f3160604a94d96a65dee567d1c
Daniel San <danielgdt88@hotmail.com>
2025-03-01 16:01:14 -0300

    refactor: I refactor the logic using the api

--- src/components/vacances/article-vacances.tsx
+++ src/components/vacances/article-vacances.tsx
@@ -3,114 +3,9 @@
 import Image from "next/image";
 import DialogVacances from "./dialog-vacandes";
 import { useState } from "react";
+import IJobs from "@/models/jobs.interface";
 
-export default function ArticleVacances({
-  contentDialog,
-}: {
-  contentDialog: string;
-}) {
-  const jobs = [
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 1,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 2,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 3,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 4,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 5,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 6,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 7,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 8,
-    },
-    {
-      "recruiter-enterprise": "Helpper - Soluções Inteligentes",
-      modality: "Remoto",
-      avatar:
-        "https://s3-alpha-sig.figma.com/img/b3d2/8e0a/7df9e0bc64043b6133afd254f7e1ea70?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DknFjseSzLrmFhb3vGob7DROSEOhP6CWyZ9koCt1ltuqBLO80UcZeWywJ73WFZV3yK2vvjo5lvDBM5n8Y205fhBSoSJ1MyvwZmJEFbybxBh1TKbPIji9rq4b5Ine8gAZTPK03r01RGOLLFqMND9m4aGXQsljO8noMLGQRIWbUMkAK3gfhh-Qi8Bol0oOCxN4RFLRBf8LTLij20bOPAYPkcbyF3IWG9A9qjJMa36NoheeSWUmdeFMRj4vnDYPxOPPZmKmPYOu7OYVk0S-Osq4HJ5Izi7cidaP84usSQ4ghVCXxnegQc533xzFw-TJFZ2DGjIATDm5WITP6Octk0a6rw__",
-      "job-title": "Desenvolvedor back-end Jr",
-      description: "João Pessoa - PB, a 12,6 Km de você. Home Office",
-      salary: "R$1.200,00",
-      especiality: "back-end",
-      id: 9,
-    },
-  ];
-
+export default function ArticleVacances({ jobs }: { jobs: IJobs[] }) {
   const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   return (
@@ -119,14 +14,13 @@ export default function ArticleVacances({
         return (
           <article key={job.id} className="bg-white p-5 rounded-xl shadow-md">
             <div className="flex gap-x-3 pb-6 items-center">
-              <div className="min-w-11">
-                <Image
-                  src={"/images/avatar.png"}
-                  width={45}
-                  height={45}
-                  alt="avatar"
-                />
-              </div>
+              <Image
+                src={job["image-recruiter"]}
+                width={45}
+                height={45}
+                alt="avatar"
+                className="min-w-12 min-h-12 rounded-full"
+              />
               <div className="flex flex-col text-nowrap">
                 <span className="font-bold text-sm">
                   {job["recruiter-enterprise"]}
@@ -135,9 +29,9 @@ export default function ArticleVacances({
               </div>
             </div>
 
-            <h1 className="text-xl font-bold pb-3">{job["job-title"]}</h1>
+            <h1 className="text-xl font-bold pb-3">{job.title}</h1>
 
-            <em>{job.description}</em>
+            <em>{job.locality}</em>
 
             <div className="flex justify-between items-center pt-5">
               <span className="font-bold">{job.salary}</span>
@@ -150,7 +44,7 @@ export default function ArticleVacances({
             </div>
             <DialogVacances
               openDialog={openDialog}
-              content={contentDialog}
+              content={job.description}
               onClose={() => setOpenDialog(!openDialog)}
             />
           </article>
