e3f015c787f0c449a7aaf7a69bfff6a779c486e5
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2025-03-15 14:31:42 -0300

    test: test metadatas

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -5,65 +5,85 @@ import Loading from "@/components/loading";
 import RelevantTopics from "@/components/relevant-topics";
 
 import "@/styles/details-post.css";
-import Head from "next/head";
 import { Post } from "@/utils/interfaces/posts";
 import { techsRelevants } from "@/utils/mock/topich-relevants";
+import { Metadata } from "next";
 
 interface IPostsDetail {
   params: Promise<{ slug: string }>;
 }
 
-export default async function Posts({ params }: IPostsDetail) {
-  const { slug } = await params;
-
-  const responsePosts = await fetch(
-    "https://api.jsonbin.io/v3/b/67be1eaaacd3cb34a8f06a64",
-    {
-      next: {
-        revalidate: 60 * 60 * 24, // 24 hours
-      },
-    }
-  ).then((res) => {
+const posts = async () =>
+  await fetch("https://api.jsonbin.io/v3/b/67be1eaaacd3cb34a8f06a64", {
+    next: {
+      revalidate: 60 * 60 * 24, // 24 hours
+    },
+  }).then((res) => {
     return res.json();
   });
 
-  const posts = responsePosts.record.posts;
+async function getPostDetails(slug: string) {
+  const responsePosts = await posts();
+  return responsePosts.record.posts.find((post: Post) => post.slug === slug);
+}
 
-  const postDetails = posts.find((post: Post) => post.slug === slug);
+export async function generateMetadata({
+  params,
+}: IPostsDetail): Promise<Metadata> {
+  const slug = (await params).slug;
+  const postDetails = await getPostDetails(slug);
+
+  if (!postDetails) {
+    return { title: "Post não encontrado" };
+  }
+
+  return {
+    title: postDetails.title,
+    description: postDetails.description,
+    openGraph: {
+      title: postDetails.title,
+      description: postDetails.description,
+      url: `https://seusite.com/posts/${slug}`,
+      type: "article",
+      images: [{ url: postDetails.image }],
+    },
+  };
+}
+
+export default async function Posts({ params }: IPostsDetail) {
+  const postDetails = await getPostDetails((await params).slug);
 
   if (!postDetails) {
     return <Loading />;
   }
 
-  return (
-    <>
-      <Head>
-        <title>{postDetails?.title}</title>
-        <meta name="description" content={postDetails.description} />
-      </Head>
-      <main className="w-full pt-9 flex max-w-page mx-auto px-4">
-        <DetailsPosts
-          author={postDetails.author.name}
-          createdAt={postDetails.createdAt}
-          image={postDetails.image}
-          data={postDetails.description}
-          className="font-bold text-base mt-2"
-          title={postDetails.title}
-        />
-        <div className="flex-col gap-y-6 items-center mb-4 md:gap-x-4 md:mb-4 hidden md:flex w-full max-w-articles-detail">
-          <LayoutArticle title="Juniando">
-            Ajudamos você Desenvolvedor Jr a conseguir sua primeira vaga no
-            mercado de trabalho, com dicas e anúncios de vagas para você
-            alavancar sua carreira.
-          </LayoutArticle>
+  const currentPosts = await posts();
 
-          <LayoutArticle title="Assuntos Relevantes">
-            <div className="mx-auto w-24 h-two-pixels rounded-2xl bg-line-blue md:m-0 md:mx-auto lg:mx-auto lg:my-1" />
-            <RelevantTopics techsRelevants={techsRelevants(posts)} />
-          </LayoutArticle>
-        </div>
-        <Bubble />
-      </main>
-    </>
+  return (
+    <main className="w-full pt-9 flex max-w-page mx-auto px-4">
+      <DetailsPosts
+        author={postDetails.author.name}
+        createdAt={postDetails.createdAt}
+        image={postDetails.image}
+        data={postDetails.description}
+        className="font-bold text-base mt-2"
+        title={postDetails.title}
+      />
+      <div className="flex-col gap-y-6 items-center mb-4 md:gap-x-4 md:mb-4 hidden md:flex w-full max-w-articles-detail">
+        <LayoutArticle title="Juniando">
+          Ajudamos você Desenvolvedor Jr a conseguir sua primeira vaga no
+          mercado de trabalho, com dicas e anúncios de vagas para você alavancar
+          sua carreira.
+        </LayoutArticle>
+
+        <LayoutArticle title="Assuntos Relevantes">
+          <div className="mx-auto w-24 h-two-pixels rounded-2xl bg-line-blue md:m-0 md:mx-auto lg:mx-auto lg:my-1" />
+          <RelevantTopics
+            techsRelevants={techsRelevants(currentPosts.record.posts)}
+          />
+        </LayoutArticle>
+      </div>
+      <Bubble />
+    </main>
   );
 }
